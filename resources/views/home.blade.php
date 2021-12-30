0fa0c6d5861bb78055912bba2fb11e42a5f16c5a
BejSuman <bejsuman5@gmail.com>
2021-12-30 20:21:50 +0530

    modify and create HTML home page Navbar

---
+++ resources/views/home.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="shortcut icon" href="../docs/pic-1.png" type="image/x-icon">
+
+    <!-- Latest compiled and minified CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/mediaQueries.css">
+
+    <!-- Latest compiled JavaScript -->
+    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="../js/modify.js"></script>
+
+    <title>Food Plaza &#127828;</title>
+</head>
+
+<body>
+
+    <!--Create Navber-->
+    <nav class="navbar navbar-expand-sm navbar-dark">
+        <div class="container-fluid">
+            <div class="navItemsCenter">
+                <a class="navbar-brand m-0" href="#">
+                    <img src="../docs/logo.png" alt="Logo" class="rounded-pill navImgLogo">
+                </a>
+            </div>
+            <div class="navItemsCenter">
+                <h1 class="text-center">Pood Plaza</h1>
+                <h4>The Family Restaurant</h4>
+            </div>
+        </div>
+    </nav>
+
+    <!--Create Navber-->
+    <nav class="navbar navbar-expand-sm sticky-top bg-dark navbar-dark py-0">
+        <div class="container-fluid ">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="mynavbar">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item mx-1">
+                        <a class="nav-link text-white px-3" href="#">HOME</a>
+                    </li>
+                    <li class="nav-item mx-1">
+                        <a class="nav-link text-white px-3" href="#">MENU</a>
+                    </li>
+                    <li class="nav-item mx-1">
+                        <a class="nav-link text-white px-3" href="#">ABOUT</a>
+                    </li>
+                    <li class="nav-item mx-1">
+                        <a class="nav-link text-white px-3" href="#">CONTACT</a>
+                    </li>
+                    <li class="nav-item dropdown mx-1">
+                        <a class="nav-link dropdown-toggle text-white px-3" href="#" role="button"
+                            data-bs-toggle="dropdown">Menu Items</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Veg Foods</a></li>
+                            <li><a class="dropdown-item" href="#">Non Veg Foods</a></li>
+                            <li><a class="dropdown-item" href="#">Drink</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ms-auto navUl">
+                    <li class="nav-item mx-1 liLeft">
+                        <a class="nav-link text-white px-3" href="#">LOG IN</a>
+                    </li>
+                    <li class="nav-item mx-1 liRight">
+                        <a class="nav-link text-white px-3" href="#">SING UP</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    
+
+
+</body>
+
+</html>
